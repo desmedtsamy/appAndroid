e08d48e9cd3cfdd8cefdb0da1eba23b27fa137bf
52854 <52854@etu.he2b.be>
2021-03-06 10:56:19 +0100

    add binding

--- projet/app/src/main/java/esi/g52854/projet/fragment_connexion.kt
+++ projet/app/src/main/java/esi/g52854/projet/fragment_connexion.kt
@@ -1,11 +1,20 @@
 
 package esi.g52854.projet
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.databinding.DataBindingUtil
+import esi.g52854.projet.databinding.ActivityMainBinding
+import esi.g52854.projet.databinding.FragmentConnexionBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,23 +27,22 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class fragment_connexion : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connexion, container, false)
+        val binding = DataBindingUtil.inflate<FragmentConnexionBinding>(inflater,
+            R.layout.fragment_connexion ,container,false)
+        binding.connexionButton.setOnClickListener{
+            checkEmail(binding.emailEdit.text.toString())
+        }
+        return binding.root
     }
+    private fun checkEmail(editText: String) {
+        val test = Patterns.EMAIL_ADDRESS.matcher(editText).matches();
+        val message = if(test)  "email valide" else "erreur : email invalide"
+        Toast.makeText(getActivity(),message , Toast.LENGTH_SHORT).show();
 
-
+    }
 }
